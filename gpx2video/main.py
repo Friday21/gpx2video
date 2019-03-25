3723a084ca337cad111959746b565be792e8dbb2
lidongyong <lidongyong@corp.netease.com>
2019-03-25 22:17:00 +0800

    first available version

--- gpx2video/main.py
+++ gpx2video/main.py
@@ -1,72 +1,102 @@
+import cv2
+import numpy as np
 import gpxpy.gpx
 import matplotlib.pyplot as plt
 from PIL import Image
 
-from gpx2video.map import get_tile_bound, merge_tile_pic, num2deg
+from gpx2video.map import get_tile_bound, merge_tile_pic, deg2pixel, wgs84togcj02, deg2num
 
 gpx_file = open('data/太子湾.gpx', 'r')
 gpx = gpxpy.parse(gpx_file)
 
 
-level = 16
+level = 20
 
 
 def draw_pics():
     points = []
     cnt = 0
+    row_start, row_end, col_start, col_end, row, col = 0, 100000000, 0, 100000000, None, None
+    img = None
     for track in gpx.tracks:
         for segment in track.segments:
+            total = len(segment.points)
             for point in segment.points:
-                points.append(point)
-                row_start, row_end, col_start, col_end = get_tile_bound(point.latitude, point.longitude, level)
-                img = merge_tile_pic(level, row_start, row_end, col_start, col_end)
-                left_top_latitude, left_top_longitude = num2deg(col_start, row_start, level)
-                right_down_latitude, right_down_longitude = num2deg(col_end + 1, row_end + 1, level)
-                row_range = left_top_latitude - right_down_latitude
-                col_range = right_down_longitude - left_top_longitude
+                lng, lat = wgs84togcj02(point.longitude, point.latitude)
+                col, row = deg2num(lat, lng, level)
+                row_range = (row_end - row_start + 1) * 256
+                col_range = (col_end - col_start + 1) * 256
+                pixel_x, pixel_y = deg2pixel(lat, lng, level)
+                x_ax = (((col - col_start) * 256) + pixel_x) / col_range * 1920
+                y_ax = (1 - ((row - row_start) * 256 + pixel_y) / row_range) * 1920
 
-                plt.axis([0, 1920, 0, 1920])
+                if x_ax < 50 or x_ax > 1800 or y_ax < 50 or y_ax > 1800:
+                    # 当坐标不合适时刷新背景图片
+                    print('refresh background')
+                    row_start, row_end, col_start, col_end, row, col = get_tile_bound(lat, lng, level)
+                    row_range = (row_end - row_start + 1) * 256
+                    col_range = (col_end - col_start + 1) * 256
+                    pixel_x, pixel_y = deg2pixel(lat, lng, level)
+                    x_ax = (((col - col_start) * 256) + pixel_x) / col_range * 1920
+                    y_ax = (1 - ((row - row_start) * 256 + pixel_y) / row_range) * 1920
+                    img = merge_tile_pic(level, row_start, row_end, col_start, col_end)
 
-                x_axes, y_axes = cal_ax(points, left_top_latitude, left_top_longitude,
-                                        right_down_latitude, right_down_longitude, col_range, row_range)
+                image = img.copy()
+                points.append({'row': row, 'col': col, 'pixel_x': pixel_x, 'pixel_y': pixel_y})
+                x_axes, y_axes = cal_ax(points, row_start, row_end, col_start, col_end, col_range, row_range)
 
-                # TODO WRONG ！ 经纬度转瓦片像素坐标是非线性的。。。。
-                x_ax = (point.longitude - left_top_longitude) / col_range * 1920
-                y_ax = (point.latitude - right_down_latitude) / row_range * 1920
-
-                plt.plot(x_axes, y_axes, 'b', markersize=10)
-                plt.plot(x_ax, y_ax, 'bo', )
+                plt.figure(figsize=(12, 12))
+                plt.axis([-1920, 1920, -1920, 1920])
+                plt.plot(x_axes, y_axes, 'b')
+                plt.plot(x_ax, y_ax, 'bo', marker="*")
                 plt.axis('off')
-                plt.savefig('tmp.png', transparent=True)
+
+                plt.savefig('tmp.png', transparent=True, bbox_inches='tight')
                 plt.close()
 
                 pil_image = Image.open('tmp.png')
+                pil_image = pil_image.resize((3840, 3840))
+                pil_image = pil_image.crop((1920, 0, 3840, 1920))
                 pil_image = pil_image.resize((1920, 1920))
 
-                img.paste(pil_image, (0, 0), mask=pil_image)
+                image.paste(pil_image, (0, 0), mask=pil_image)
                 cnt += 1
-                img.show()
-                print(left_top_latitude, left_top_longitude, right_down_latitude, right_down_longitude,
-                      point.latitude, point.longitude)
-                break
                 # TODO crop to 1920 * 1080
-                img.save('output/{}.png'.format(cnt))
-                print('save', cnt)
+                image.save('output/{:0>6}.png'.format(cnt))
+                print('save {} of {}'.format(cnt, total))
 
 
-def cal_ax(points, left_top_latitude, left_top_longitude, right_down_latitude,
-           right_down_longitude, col_range, row_range):
+def cal_ax(points, row_start, row_end, col_start, col_end, col_range, row_range):
     x_axes = []
     y_axes = []
     for point in points:
-        if right_down_latitude <= point.latitude <= left_top_latitude and \
-                left_top_longitude <= point.longitude <= right_down_longitude:
-            x_ax = (point.longitude - left_top_longitude) / col_range * 1920
-            y_ax = (point.latitude - right_down_latitude) / row_range * 1920
-            x_axes.append(x_ax)
-            y_axes.append(y_ax)
+        x_ax = ((point['col'] - col_start) * 256 + point['pixel_x']) / col_range * 1920
+        y_ax = (1 - ((point['row'] - row_start) * 256 + point['pixel_y']) / row_range) * 1920
+        x_axes.append(x_ax)
+        y_axes.append(y_ax)
     return x_axes, y_axes
 
 
+def rotate_img(img):
+    pts1 = np.float32([[0, 0], [1920, 0], [0, 1920], [1920, 1920]])  # 左上，右上，左下，右下
+    pts2 = np.float32([[0, 200], [1920, 200], [0, 1720], [1920, 1720]])  # 变换后
+
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    dst = cv2.warpPerspective(img, M, (1920, 1920))
+
+    plt.subplot(121), plt.imshow(img), plt.title('Input')
+    plt.subplot(122), plt.imshow(dst), plt.title('Output')
+    plt.show()
+    plt.savefig('400_rotate.png')
+
+
 if __name__ == '__main__':
+    # out_path = '/Users/friday/work/friday/gpx-to-video/gpx2video/output'
+    # img = cv2.imread('{}/400.png'.format(out_path))
+    # rotate_img(img)
+    # ffmpeg -start_number 001 -r 10 -i output/%6d.png -pix_fmt yuv420p test5.mp4
     draw_pics()
+    # img = Image.open('together.png')
+    # img = img.resize((3840, 3840))
+    # img = img.crop((1920, 0, 3840, 1920))
+    # img.show()
--- gpx2video/map.py
+++ gpx2video/map.py
@@ -1,6 +1,5 @@
 import os
 import math
-import time
 
 from PIL import Image
 
@@ -8,16 +7,20 @@ from gpx2video.config import map_path
 
 
 tile_size = 256  # 瓦片大小256*256
+x_pi = 3.14159265358979324 * 3000.0 / 180.0
+pi = 3.1415926535897932384626  # π
+a = 6378245.0  # 长半轴
+ee = 0.00669342162296594323  # 扁率
 
 
 def get_tile_bound(lat, lon, level):
     """获取瓦片的编号范围"""
     col, row = deg2num(lat, lon, level)  # 中心
-    row_start = row - 3
-    row_end = row + 2
-    col_start = col - 3
-    col_end = col + 2
-    return row_start, row_end, col_start, col_end
+    row_start = row - 6
+    row_end = row + 5
+    col_start = col - 6
+    col_end = col + 5
+    return row_start, row_end, col_start, col_end, row, col
 
 
 def merge_tile_pic(level, row_start, row_end, col_start, col_end, filename=None):
@@ -29,7 +32,6 @@ def merge_tile_pic(level, row_start, row_end, col_start, col_end, filename=None)
     :param col_end: 瓦片列号终点
     :return: 合成好的图片
     """
-    result_filepath = filename or '{}.png'.format(int(time.time()))
 
     tile_dir = os.path.join(map_path, str(level))
 
@@ -95,6 +97,95 @@ def deg2pixel(lat_deg, lon_deg, zoom):
     pixel_x = int((lon_deg + 180.0) / 360.0 * n * 256 % 256)
     pixel_y = int((1 - math.log(math.tan(lat_ang) + 1/math.cos(lat_ang))/(2*math.pi)) * n * 256 % 256)
     return pixel_x, pixel_y
+
+
+def deg2pic(lat_deg, lon_deg, zoom):
+    col, row = deg2num(lat_deg, lon_deg, zoom)
+    path = os.path.join(map_path, str(zoom), str(col), '{}.jpg'.format(row))
+    image = Image.open(path)
+    image.show()
+
+
+def wgs84togcj02(lng, lat):
+    """
+    WGS84转GCJ02(火星坐标系)
+    :param lng:WGS84坐标系的经度
+    :param lat:WGS84坐标系的纬度
+    :return:
+    """
+    if out_of_china(lng, lat):  # 判断是否在国内
+        return lng, lat
+    dlat = transformlat(lng - 105.0, lat - 35.0)
+    dlng = transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = lat + dlat
+    mglng = lng + dlng
+    return [mglng, mglat]
+
+
+def gcj02towgs84(lng, lat):
+    """
+    GCJ02(火星坐标系)转GPS84
+    :param lng:火星坐标系的经度
+    :param lat:火星坐标系纬度
+    :return:
+    """
+    if out_of_china(lng, lat):
+        return lng, lat
+    dlat = transformlat(lng - 105.0, lat - 35.0)
+    dlng = transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = lat + dlat
+    mglng = lng + dlng
+    return [lng * 2 - mglng, lat * 2 - mglat]
+
+
+def transformlat(lng, lat):
+    ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+        0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lat * pi) + 40.0 *
+            math.sin(lat / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * math.sin(lat / 12.0 * pi) + 320 *
+            math.sin(lat * pi / 30.0)) * 2.0 / 3.0
+    return ret
+
+
+def transformlng(lng, lat):
+    ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+        0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lng * pi) + 40.0 *
+            math.sin(lng / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 *
+            math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
+    return ret
+
+
+def out_of_china(lng, lat):
+    """
+    判断是否在国内，不在国内不做偏移
+    :param lng:
+    :param lat:
+    :return:
+    """
+    if lng < 72.004 or lng > 137.8347:
+        return True
+    if lat < 0.8293 or lat > 55.8271:
+        return True
+    return False
 
 
 if __name__ == '__main__':
